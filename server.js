84e9af9dd269996d89f9ba719f423e35772782f7
tauisf-jawaid <tausifjawaid6@gmail.com>
2023-06-16 14:13:31 +0530

    some code changes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const productsRouts = require('./routes/products')
 
 const fs = require('fs');
 const cors = require('cors')
