3d90fb86bf463063d9d52c8fc28d098c2c76a64e
puneet <puneetsingh28897@gmail.com>
2023-06-19 17:53:10 +0530

    validate  code  added

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,14 +3,16 @@ const axios = require("axios");
 require('dotenv').config();
 const studentSchema = require('../models/workoutModel');
 const { error } = require("console");
-const { nameValidation } = require("../helper/validation");
+const {validate_data, validateUser}= require("../helper/validation");
+
+
 
 const token = process.env.ACCESS_TOKEN;
 //get all workouts
 
 const getProduct = async (req, res) => {
-
-  console.log(nameValidation("puneet singh"));
+ const dataname="puneet singh";
+  console.log(nameValidation(dataname));
     studentSchema.find((error, data) => {
       try {
         if (error) {
@@ -27,23 +29,35 @@ const getProduct = async (req, res) => {
 };
 
 // create new metafields for specific product
-const createProducts = async (req, res) => {
+const createProducts = 
+async (req, res) => {
+// validate 
+ 
+    const value = validateUser(req.body);
+   if(value.error){
+    console.log(value.error);
+    res.json(value.error);
+   }else{
+    studentSchema.create(req.body, (error, data) => {
+      try {
+        if (error) {
+          //return res.json(error)
+          return next(error)
+        } else {
+          console.log(data)
+          res.json(data)
+        } 
+      } catch (error) {
+        res.json(error)
+      }
+      
+    })
+   }
+
+
 
  
-  studentSchema.create(req.body, (error, data) => {
-    try {
-      if (error) {
-        //return res.json(error)
-        return next(error)
-      } else {
-        console.log(data)
-        res.json(data)
-      } 
-    } catch (error) {
-      res.json(error)
-    }
-    
-  })
+ 
 };
 
 const deleteProduct = async (req,res) => {
--- helper/validation.js
+++ helper/validation.js
@@ -1,30 +1,64 @@
-const fs = require("fs");
-const axios = require("axios");
-require('dotenv').config();
-var validator = require('validator');
+// const fs = require("fs");
+// const axios = require("axios");
+// require('dotenv').config();
+// var validator = require('validator');
 
-const { error } = require("console");
+// const { error } = require("console");
 
-const token = process.env.ACCESS_TOKEN;
+// const token = process.env.ACCESS_TOKEN;
 
 
-function nameValidation (name) {
+// function nameValidation (name) {
 
     
-    return name;
+//     return name.length;
     
-};
+// };
 
 
-function mailValidation(mail){
- return isEmail(mail)
+// function mailValidation(mail){
+//  return isEmail(mail)
  
-};
+// };
 
    
 
 
+// module.exports = {
+//   nameValidation,
+//   mailValidation
+// };
+
+
+const Joi = require('joi');
+
+const validate_data =(data)=> Joi.object({
+  name:Joi.string().required(),
+  email: Joi.string()
+  .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+  rollno:Joi.string().required()
+
+
+
+
+})
+
+function validateUser(user)
+{
+    const JoiSchema = Joi.object({
+      
+        name: Joi.string().min(5).max(30).required(),
+                    
+        email: Joi.string().email() .min(5).max(50).optional(), 
+        rollno:Joi.string().required()
+    }).options({ abortEarly: false });
+  
+    return JoiSchema.validate(user)
+}
+
+
+
 module.exports = {
-  nameValidation,
-  mailValidation
+  validate_data,
+  validateUser
 };
